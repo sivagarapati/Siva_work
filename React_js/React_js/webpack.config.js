6349f76abfe77e3381f867600149d513123e5969
sivagarapati <siva.gsk333@gmail.com>
2017-04-13 01:32:08 +0530

    Add files via upload

---
+++ React_js/React_js/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    entry: './script.js',
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'transpiled.js'
+    },
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+
+        ]
+    }
+}
