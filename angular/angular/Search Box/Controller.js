6da1f0e031f96a8d0345fe3cc6565543ecaad9d0
sivagarapati <siva.gsk333@gmail.com>
2017-04-13 01:09:50 +0530

    Add files via upload

---
+++ angular/angular/Search Box/Controller.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('myApp', []);
+
+
+
+app.controller("people", function($scope, $http) {
+    var self = this;
+    //$http.get("http://127.0.0.1/../data.json")
+    $http.get("http://localhost:3000/records")
+        .then(function(response) {
+            $scope.persons = response.data;
+        });
+});
